4b33cf63e0b53b333aa28b6105dd600bf7f6ae67
Victor H. T. Filho <victorhugo.tozzo@gmail.com>
2023-07-31 16:57:51 -0300

    responsividade

--- my-page/src/app/layout.js
+++ my-page/src/app/layout.js
@@ -1,12 +1,18 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={`overflow-x-hidden ${inter.className} `}>{children}</body>
+      {/* Use Head to add meta tags */}
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className={`overflow-x-hidden ${inter.className} `}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
