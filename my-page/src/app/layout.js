a98f8cc745d5f96f9a6b23be0ee8bfe2b96127bf
Victor H. T. Filho <victorhugo.tozzo@gmail.com>
2023-08-01 02:00:46 -0300

    Update layout.js

--- my-page/src/app/layout.js
+++ my-page/src/app/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <title>Torugo-Tozzo</title>
-      <link rel="icon" href="./favicon.ico" />
+      <link rel="icon" href="/favicon.ico" sizes="any"/>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
